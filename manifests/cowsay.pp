290a90164cae78cd8a0b2e72613fd5d5142c2614
zillion504 <70014062+zillion504@users.noreply.github.com>
2022-09-13 20:09:47 -0700

    add cowsay to path

--- manifests/cowsay.pp
+++ manifests/cowsay.pp
@@ -2,6 +2,11 @@ package { 'cowsay':
   ensure => installed,
 }
 
+file { '/etc/profile.d/cowsay-path.sh':
+  mode    => '644',
+  content => 'PATH=$PATH:/usr/games/',
+}
+
 file { '/root/hello2.txt':
   ensure  => present,
   content => "cowsay \"hello world!\"\n",
